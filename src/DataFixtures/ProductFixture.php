c803154d8d5053f528e720e6b863ee99cb2022f4
alienorG <alienor@laplumeblanche.com>
2019-06-12 14:23:17 +0200

    connection BDD v2

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -4,7 +4,82 @@
 namespace App\DataFixtures;
 
 
-class ProductFixture
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ProductFixture extends Fixture
 {
 
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Product 1
+        $product = new Product();
+        $product->setTitle('Tapis en laine')
+            ->setPrice('20')
+            ->setDescription('<p>Vintage from the 1970s<br>Tapis Jacquard Vintage Jacquard Spread Bedspread on the sofa 
+                                        Double-sided woven bedspread Decoration Ukrainian Made in USSR <br>
+                                        Defects: No<br>
+                                        Size: 194cm * 133cm (76 "* 52")<br>
+                                        Please, remember- this item is vintage, it was in use. Vintage is rarely perfect, 
+                                        items can have marks of time and using. However, items sold will be clean and usable.
+                                        If you have any questions, write to me and I will make for you the photos or dimensions that you want.<br>
+                                        Thank you for visiting!</p>')
+            ->setPicture('tapis1.jpg')
+            ->setDateCreation(new \DateTime())
+            ->setIdSeller('1')
+            ->setSlug('tapis-en-laine')
+            ->setSpotlight(1);
+        $manager->persist($product);
+
+        // Product 2
+        $product = new Product();
+        $product->setTitle('Jouet en laine pour chat')
+            ->setPrice('5')
+            ->setDescription('<p>Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. 
+                                        Caeli dare potuit isti suspicioni locum, fuit primum ipsius pudore, deinde 
+                                           etiam patris diligentia disciplinaque munita. Qui ut huic virilem togam 
+                                           deditšnihil dicam hoc loco de me; tantum sit, quantum vos existimatis;</p>')
+            ->setPicture('jouet-chat.jpg')
+            ->setDateCreation(new \DateTime())
+            ->setIdSeller('1')
+            ->setSlug('jouet-chat')
+            ->setSpotlight(1);
+        $manager->persist($product);
+
+        // Product 3
+        $product = new Product();
+        $product->setTitle('Tapis en laine noir et blanc')
+            ->setPrice('40')
+            ->setDescription('<p>Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. 
+                                        Caeli dare potuit isti suspicioni locum, fuit primum ipsius pudore, deinde 
+                                           etiam patris diligentia disciplinaque munita. Qui ut huic virilem togam 
+                                           deditšnihil dicam hoc loco de me; tantum sit, quantum vos existimatis;</p>')
+            ->setPicture('tapis-2.jpg')
+            ->setDateCreation(new \DateTime())
+            ->setIdSeller('1')
+            ->setSlug('tapis-en-laine-noir-et-blanc')
+            ->setSpotlight(1);
+        $manager->persist($product);
+
+        // Product 4
+        $product = new Product();
+        $product->setTitle('Panier en osier')
+            ->setPrice('30')
+            ->setDescription('<p>Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. 
+                                        Caeli dare potuit isti suspicioni locum, fuit primum ipsius pudore, deinde 
+                                           etiam patris diligentia disciplinaque munita. Qui ut huic virilem togam 
+                                           deditšnihil dicam hoc loco de me; tantum sit, quantum vos existimatis;</p>')
+            ->setPicture('panier-1.jpg')
+            ->setDateCreation(new \DateTime())
+            ->setIdSeller('1')
+            ->setSlug('panier-en-osier')
+            ->setSpotlight(1);
+        $manager->persist($product);
+    }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $zip_code;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $inscription_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(int $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    public function getInscriptionDate(): ?\DateTimeInterface
+    {
+        return $this->inscription_date;
+    }
+
+    public function setInscriptionDate(\DateTimeInterface $inscription_date): self
+    {
+        $this->inscription_date = $inscription_date;
+
+        return $this;
+    }
+}
